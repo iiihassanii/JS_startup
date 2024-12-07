799dc8a74d80476abb3b3b9f601e29414ee2ecb8
iiihassanii <hassanAbdelazim.max@gmail.com>
2024-12-07 17:22:47 +0300

    conactenation

---
+++ 03_con_data_type/js/main.js
@@ -0,0 +1,11 @@
+var name = "Hassan";
+var name = "Hi";
+let age = 25;
+// let age = 25; X cant fo that
+
+
+
+const v = 1;
+console.log(name);
+console.log(age);
+console.log(v);
---
+++ 03_conactenation/js/main.js
@@ -0,0 +1,4 @@
+let name = "hassan";
+let age = 25;
+console.log("my name is " + name + "and I'm "+ age + " years old");
+console.log(`my name is ${name} and I'm ${age} years old`);
