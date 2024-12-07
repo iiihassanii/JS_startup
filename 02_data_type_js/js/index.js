439d91199ec14a797b3a72472c2d13b78faafef1
iiihassanii <hassanAbdelazim.max@gmail.com>
2024-12-07 15:39:37 +0300

    Data type in js

---
+++ 02_data_type_js/js/index.js
@@ -0,0 +1,23 @@
+
+document.write("<h1>Hello there</h1>");
+document.querySelector("h1").style.color = "red";
+//string
+document.write(typeof "<h1>Hello there</h1>");
+document.write("<br>");
+//number
+document.write(1);
+document.write("<br>");
+document.write(typeof 1);
+document.write("<br>");
+//null bug?
+document.write(typeof null);
+document.write("<br>");
+// let
+let x;
+document.write(typeof x);
+document.write("<br>");
+// let number
+let y = 5;
+document.write(typeof y);
+document.write("<br>");
+
